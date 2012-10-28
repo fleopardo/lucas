ad447365c68ead16a1887ba012957040a18a01d7
fleopardo <fleopardo@gmail.com>
2012-10-28 20:02:41 -0300

    se commitea contacto

--- js/global.js
+++ js/global.js
@@ -1,7 +1,24 @@
 $(function(){
 	
 	
+	var options = {
+        zoom: 15,
+        center: new google.maps.LatLng(-34.58, -58.47),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+		mapTypeControl: false,
+		navigationControl: false,
+		streetViewControl: false
+    };
+    var map = new google.maps.Map(document.getElementById('map'), options);
 	
+	new google.maps.Marker({
+        position: map.getCenter()
+        , map: map
+        , title: 'Pulsa aquí'
+        , icon: '../img/fd_map-flag.png'
+        , cursor: 'default'
+        , draggable: true
+    });
 
 	
 }); 
