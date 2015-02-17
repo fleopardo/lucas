db76c838c56f0b1f8b27a8dd08e6926394b5b380
fleopardo <fleopardo@gmail.com>
2015-02-17 19:37:06 -0300

    footer

--- js/bienvenidos.js
+++ js/bienvenidos.js
@@ -5,4 +5,9 @@
 		$('.bienvenidos .bid article').toggleClass('open');
 	});
 
+	$('footer a.ver-mas-footer').on('click', function (event) {
+		event.preventDefault();
+		$(this).toggleClass('open');
+	});
+
 }());
