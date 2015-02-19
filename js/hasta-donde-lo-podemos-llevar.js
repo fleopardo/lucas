c3652ef86dc05c7b023079a59ae49c553e6087de
sleopardo <santiago.leopardo@mercadolibre.com>
2015-02-19 15:30:46 -0300

    comente animacion

--- js/hasta-donde-lo-podemos-llevar.js
+++ js/hasta-donde-lo-podemos-llevar.js
@@ -1,46 +1,32 @@
 ;(function (win) {
-
-	animationController.addTween(
-		'.hasta-donde-lo-podemos-llevar',
-	    TweenMax.to(
-	    	$('.hasta-donde-lo-podemos-llevar .container'),
-	    	.5,
-	    	{css: {opacity:0.5}}, {css: {opacity:1}}
-	    ),
-	    300
-	);
-
-	animationController.addTween(
-		'.hasta-donde-lo-podemos-llevar',
-	    TweenMax.to(
-	    	$('.hasta-donde-lo-podemos-llevar .container'),
-	    	.5,
-	    	{css: {transform: 'scale(0.9)'}}, {css: {transform: 'scale(1)'}}
-	    ),
-	    300
-	);
-
-	animationController.addTween(
-		'.hasta-donde-lo-podemos-llevar',
-		TweenMax.fromTo(
-			$('.hasta-donde-lo-podemos-llevar .container'),
-			3,
-	        {css:{top: 0}}, {css:{top: 100}}
-	    ),
-	    500
-	);
+	/* TODO: DESCOMENTAR DESPUES DE ENTREGA PARCIAL */
 
 	// animationController.addTween(
-	// 	'.hasta-donde-lo-podemos-llevar .logo',
+	// 	'.hasta-donde-lo-podemos-llevar',
+	//     TweenMax.to(
+	//     	$('.hasta-donde-lo-podemos-llevar .container'),
+	//     	.5,
+	//     	{css: {opacity:0.5}}, {css: {opacity:1}}
+	//     ),
+	//     300
+	// );
+
+	// animationController.addTween(
+	// 	'.hasta-donde-lo-podemos-llevar',
+	//     TweenMax.to(
+	//     	$('.hasta-donde-lo-podemos-llevar .container'),
+	//     	.5,
+	//     	{css: {transform: 'scale(0.9)'}}, {css: {transform: 'scale(1)'}}
+	//     ),
+	//     300
+	// );
+
+	// animationController.addTween(
+	// 	'.hasta-donde-lo-podemos-llevar',
 	// 	TweenMax.fromTo(
-	// 		$('.hasta-donde-lo-podemos-llevar .logo'),
-	// 		.5,
-	//         {css:{
-	//         	top: 30
- //       		}},
- //       		{css:{
- //       			top: 200
-	//        	}}
+	// 		$('.hasta-donde-lo-podemos-llevar .container'),
+	// 		3,
+	//         {css:{top: 0}}, {css:{top: 100}}
 	//     ),
 	//     500
 	// );
