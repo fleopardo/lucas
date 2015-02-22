1fa05c2d4f8f364959eb69bc55281e20371e57fe
sleopardo <santiago.leopardo@mercadolibre.com>
2015-02-22 18:12:52 -0300

    animacion pantalla gestion

--- js/gestion-del-conocimiento.js
+++ js/gestion-del-conocimiento.js
@@ -1,10 +1,20 @@
 ;(function (win) {
 	animationController.addTween(
 		'.gestion-del-conocimiento',
-	    TweenMax.to(
-	    	$('.gestion-del-conocimiento .container'),
+	    TweenMax.fromTo(
+	    	$('.gestion-del-conocimiento .peter'),
 	    	.5,
-	    	{css: {opacity:0.5}}, {css: {opacity:1}}
+	    	{css: {width: '60vw'}}, {css: {width:'40vw'}}
+	    ),
+	    300
+	);
+
+	animationController.addTween(
+		'.gestion-del-conocimiento',
+	    TweenMax.fromTo(
+	    	$('.gestion-del-conocimiento .gary'),
+	    	.5,
+	    	{css: {width: '40vw'}}, {css: {width:'60vw'}}
 	    ),
 	    300
 	);
@@ -12,21 +22,31 @@
 	animationController.addTween(
 		'.gestion-del-conocimiento',
 	    TweenMax.to(
-	    	$('.gestion-del-conocimiento .container'),
+	    	$('.gestion-del-conocimiento .peter .container'),
 	    	.5,
-	    	{css: {transform: 'scale(0.9)'}}, {css: {transform: 'scale(1)'}}
+	    	{css: {opacity: 0}}, {css: {opacity: 1}}
 	    ),
 	    300
 	);
 
-	animationController.addTween(
-		'.gestion-del-conocimiento',
-		TweenMax.fromTo(
-			$('.gestion-del-conocimiento .container'),
-			3,
-	        {css:{top: 0}}, {css:{top: 100}}
-	    ),
-	    500
-	);
+	// animationController.addTween(
+	// 	'.hasta-donde-lo-podemos-llevar',
+	//     TweenMax.to(
+	//     	$('.hasta-donde-lo-podemos-llevar .container'),
+	//     	.5,
+	//     	{css: {transform: 'scale(0.9)'}}, {css: {transform: 'scale(1)'}}
+	//     ),
+	//     300
+	// );
+
+	// animationController.addTween(
+	// 	'.hasta-donde-lo-podemos-llevar',
+	// 	TweenMax.fromTo(
+	// 		$('.hasta-donde-lo-podemos-llevar .container'),
+	// 		3,
+	//         {css:{top: 0}}, {css:{top: 100}}
+	//     ),
+	//     500
+	// );
 
 }(window));
