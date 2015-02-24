7bec82162291707d4e74375200cb7c3ca80d26a7
sleopardo <santiago.leopardo@mercadolibre.com>
2015-02-24 00:48:18 -0300

    gira la cruz de cerrar navegacion mobile

--- js/gestion-del-conocimiento.js
+++ js/gestion-del-conocimiento.js
@@ -1,52 +1,32 @@
-// ;(function (win) {
-// 	animationController.addTween(
-// 		'.gestion-del-conocimiento',
-// 	    TweenMax.fromTo(
-// 	    	$('.gestion-del-conocimiento .peter'),
-// 	    	.5,
-// 	    	{css: {width: '60vw'}}, {css: {width:'40vw'}}
-// 	    ),
-// 	    300
-// 	);
+;(function (win) {
+	animationController.addTween(
+		'.gestion-del-conocimiento',
+	    TweenMax.fromTo(
+	    	$('.gestion-del-conocimiento .peter'),
+	    	.5,
+	    	{css: {width: '60vw'}}, {css: {width:'40vw'}}
+	    ),
+	    300
+	);
 
-// 	animationController.addTween(
-// 		'.gestion-del-conocimiento',
-// 	    TweenMax.fromTo(
-// 	    	$('.gestion-del-conocimiento .gary'),
-// 	    	.5,
-// 	    	{css: {width: '40vw'}}, {css: {width:'60vw'}}
-// 	    ),
-// 	    300
-// 	);
+	animationController.addTween(
+		'.gestion-del-conocimiento',
+	    TweenMax.fromTo(
+	    	$('.gestion-del-conocimiento .gary'),
+	    	.5,
+	    	{css: {width: '40vw'}}, {css: {width:'60vw'}}
+	    ),
+	    300
+	);
 
-// 	animationController.addTween(
-// 		'.gestion-del-conocimiento',
-// 	    TweenMax.to(
-// 	    	$('.gestion-del-conocimiento .peter .container'),
-// 	    	.5,
-// 	    	{css: {opacity: 0}}, {css: {opacity: 1}}
-// 	    ),
-// 	    300
-// 	);
+	animationController.addTween(
+		'.gestion-del-conocimiento',
+	    TweenMax.to(
+	    	$('.gestion-del-conocimiento .peter .container'),
+	    	.5,
+	    	{css: {opacity: 0}}, {css: {opacity: 1}}
+	    ),
+	    300
+	);
 
-// 	// animationController.addTween(
-// 	// 	'.hasta-donde-lo-podemos-llevar',
-// 	//     TweenMax.to(
-// 	//     	$('.hasta-donde-lo-podemos-llevar .container'),
-// 	//     	.5,
-// 	//     	{css: {transform: 'scale(0.9)'}}, {css: {transform: 'scale(1)'}}
-// 	//     ),
-// 	//     300
-// 	// );
-
-// 	// animationController.addTween(
-// 	// 	'.hasta-donde-lo-podemos-llevar',
-// 	// 	TweenMax.fromTo(
-// 	// 		$('.hasta-donde-lo-podemos-llevar .container'),
-// 	// 		3,
-// 	//         {css:{top: 0}}, {css:{top: 100}}
-// 	//     ),
-// 	//     500
-// 	// );
-
-// }(window));
+}(window));
