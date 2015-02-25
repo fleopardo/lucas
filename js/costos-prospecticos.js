c96c0872772142c645d2066a29cd5028e2aa3d78
sleopardo <santiago.leopardo@mercadolibre.com>
2015-02-25 00:59:50 -0300

    un poco de responsive

--- js/costos-prospecticos.js
+++ js/costos-prospecticos.js
@@ -1,32 +1,35 @@
 ;(function (win) {
-	animationController.addTween(
-		'.costos-prospecticos',
-	    TweenMax.fromTo(
-	    	$('.costos-prospecticos .gary'),
-	    	.5,
-	    	{css: {width: '60vw'}}, {css: {width:'40vw'}}
-	    ),
-	    300
-	);
 
-	animationController.addTween(
-		'.costos-prospecticos',
-	    TweenMax.fromTo(
-	    	$('.costos-prospecticos .peter'),
-	    	.5,
-	    	{css: {width: '40vw'}}, {css: {width:'60vw'}}
-	    ),
-	    300
-	);
+	if ($(window).width > 720) {
+		animationController.addTween(
+			'.costos-prospecticos',
+		    TweenMax.fromTo(
+		    	$('.costos-prospecticos .gary'),
+		    	.5,
+		    	{css: {width: '60vw'}}, {css: {width:'40vw'}}
+		    ),
+		    300
+		);
 
-	animationController.addTween(
-		'.costos-prospecticos',
-	    TweenMax.to(
-	    	$('.costos-prospecticos .gary .container'),
-	    	.5,
-	    	{css: {opacity: 0}}, {css: {opacity: 1}}
-	    ),
-	    300
-	);
+		animationController.addTween(
+			'.costos-prospecticos',
+		    TweenMax.fromTo(
+		    	$('.costos-prospecticos .peter'),
+		    	.5,
+		    	{css: {width: '40vw'}}, {css: {width:'60vw'}}
+		    ),
+		    300
+		);
+
+		animationController.addTween(
+			'.costos-prospecticos',
+		    TweenMax.to(
+		    	$('.costos-prospecticos .gary .container'),
+		    	.5,
+		    	{css: {opacity: 0}}, {css: {opacity: 1}}
+		    ),
+		    300
+		);
+	}
 
 }(window));
--- js/gestion-del-conocimiento.js
+++ js/gestion-del-conocimiento.js
@@ -1,32 +1,35 @@
 ;(function (win) {
-	animationController.addTween(
-		'.gestion-del-conocimiento',
-	    TweenMax.fromTo(
-	    	$('.gestion-del-conocimiento .peter'),
-	    	.5,
-	    	{css: {width: '60vw'}}, {css: {width:'40vw'}}
-	    ),
-	    300
-	);
+	if ($(window).width > 720) {
 
-	animationController.addTween(
-		'.gestion-del-conocimiento',
-	    TweenMax.fromTo(
-	    	$('.gestion-del-conocimiento .gary'),
-	    	.5,
-	    	{css: {width: '40vw'}}, {css: {width:'60vw'}}
-	    ),
-	    300
-	);
+		animationController.addTween(
+			'.gestion-del-conocimiento',
+		    TweenMax.fromTo(
+		    	$('.gestion-del-conocimiento .peter'),
+		    	.5,
+		    	{css: {width: '60vw'}}, {css: {width:'40vw'}}
+		    ),
+		    300
+		);
 
-	animationController.addTween(
-		'.gestion-del-conocimiento',
-	    TweenMax.to(
-	    	$('.gestion-del-conocimiento .peter .container'),
-	    	.5,
-	    	{css: {opacity: 0}}, {css: {opacity: 1}}
-	    ),
-	    300
-	);
+		animationController.addTween(
+			'.gestion-del-conocimiento',
+		    TweenMax.fromTo(
+		    	$('.gestion-del-conocimiento .gary'),
+		    	.5,
+		    	{css: {width: '40vw'}}, {css: {width:'60vw'}}
+		    ),
+		    300
+		);
+
+		animationController.addTween(
+			'.gestion-del-conocimiento',
+		    TweenMax.to(
+		    	$('.gestion-del-conocimiento .peter .container'),
+		    	.5,
+		    	{css: {opacity: 0}}, {css: {opacity: 1}}
+		    ),
+		    300
+		);
+	}
 
 }(window));
