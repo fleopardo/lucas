5bbfae668682308a1a66c3f3bf18c82d2edd5b09
fleopardo <fleopardo@gmail.com>
2015-02-25 21:53:33 -0300

    responsive

--- js/nuestro-metodo-en-la-practica.js
+++ js/nuestro-metodo-en-la-practica.js
@@ -3,7 +3,7 @@
 	$('.nuestro-metodo-en-la-practica .content-nav .owl-carousel').owlCarousel({
 	    //autoPlay: 3000, //Set AutoPlay to 3 seconds
 	    items : 1, // max items shown per page
-	    itemsCustom: [[0, 1], [320, 1], [480, 1], [768, 1]], // [min-screen-width, items-to-show]
+	    singleItem: true,
 	    navigation: true,
 	    slideSpeed: 1000,
 	    navigationText : ['<a href="javascript:void(0);" title="" class="link-square"><div class="square-rotate"></div><span>Prev</span></a>','<a href="javascript:void(0);" title="" class="link-square"><div class="square-rotate"></div><span>Next</span></a>']
--- js/somos-salazar-bogoya.js
+++ js/somos-salazar-bogoya.js
@@ -17,55 +17,56 @@
 	cxtBogoya.strokeStyle = "#8c8c8c";
 	cxtBogoya.stroke();
 
+	if ($(window).width > 720) {
+		animationController.addTween(
+			'.somos-salazar-bogoya',
+		    TweenMax.to(
+		    	$('.somos-salazar-bogoya .container-text'),
+		    	.5,
+		    	{css: {opacity:0.2}}, {css: {opacity:1}}
+		    ),
+		    300
+		);
 
-	animationController.addTween(
-		'.somos-salazar-bogoya',
-	    TweenMax.to(
-	    	$('.somos-salazar-bogoya .container-text'),
-	    	.5,
-	    	{css: {opacity:0.2}}, {css: {opacity:1}}
-	    ),
-	    300
-	);
+		animationController.addTween(
+			'.somos-salazar-bogoya',
+		    TweenMax.to(
+		    	$('.somos-salazar-bogoya .container-text'),
+		    	.5,
+		    	{css: {transform: 'scale(0.9)'}}, {css: {transform: 'scale(1)'}}
+		    ),
+		    300
+		);
 
-	animationController.addTween(
-		'.somos-salazar-bogoya',
-	    TweenMax.to(
-	    	$('.somos-salazar-bogoya .container-text'),
-	    	.5,
-	    	{css: {transform: 'scale(0.9)'}}, {css: {transform: 'scale(1)'}}
-	    ),
-	    300
-	);
+		animationController.addTween(
+			'.somos-salazar-bogoya',
+			TweenMax.fromTo(
+				$('.somos-salazar-bogoya .container-text'),
+				3,
+		        {css:{top: 0}}, {css:{top: 100}}
+		    ),
+		    500
+		);
 
-	animationController.addTween(
-		'.somos-salazar-bogoya',
-		TweenMax.fromTo(
-			$('.somos-salazar-bogoya .container-text'),
-			3,
-	        {css:{top: 0}}, {css:{top: 100}}
-	    ),
-	    500
-	);
+		animationController.addTween(
+			'.somos-salazar-bogoya',
+			TweenMax.fromTo(
+				$('.somos-salazar-bogoya .names.alfredo'),
+				3,
+		        {css:{top: 168}}, {css:{top: 80}}
+		    ),
+		    1000
+		);
 
-	animationController.addTween(
-		'.somos-salazar-bogoya',
-		TweenMax.fromTo(
-			$('.somos-salazar-bogoya .names.alfredo'),
-			3,
-	        {css:{top: 168}}, {css:{top: 80}}
-	    ),
-	    1000
-	);
+		animationController.addTween(
+			'.somos-salazar-bogoya',
+			TweenMax.fromTo(
+				$('.somos-salazar-bogoya .names.nubia'),
+				3,
+		        {css:{top: 340}}, {css:{top: 250}}
+		    ),
+		    1000
+		);
 
-	animationController.addTween(
-		'.somos-salazar-bogoya',
-		TweenMax.fromTo(
-			$('.somos-salazar-bogoya .names.nubia'),
-			3,
-	        {css:{top: 340}}, {css:{top: 250}}
-	    ),
-	    1000
-	);
-
+	}
 }(window));
