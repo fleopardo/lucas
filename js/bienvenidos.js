34af771394f8b2bfd6b416a4dc57fc172cdb2de3
fleopardo <fleopardo@gmail.com>
2015-02-28 13:20:37 -0300

    animacion nueva footer, bajar Y en pantalla bienvenida

--- js/bienvenidos.js
+++ js/bienvenidos.js
@@ -1,7 +1,7 @@
 ;(function(win) {
 
-	var iconFooterTrigger = $('footer a.ver-mas-footer'),
-		footerContent = $('footer > div');
+	var iconFooterTrigger = $('a#ver-mas-footer'),
+		footerContent = $('footer');
 
 	$('.bienvenidos .bid a').on('click', function (event) {
 		event.preventDefault();
@@ -11,7 +11,17 @@
 	iconFooterTrigger.on('click', function (event) {
 		event.preventDefault();
 		$(this).toggleClass('open');
-		footerContent.fadeToggle();
+		if( !$(this).hasClass('open') ){
+			footerContent.stop(true,true).animate({
+				left: '-100%',
+				right: '100%'
+			}, 1000, "linear");
+		}else{
+			footerContent.stop(true,true).animate({
+				left: '0',
+				right: '0'
+			}, 1000, "linear");
+		}
 	});
 
 }(window));
