7f1e58868bbb0a4c647edb583673a625461187f1
fleopardo <fleopardo@gmail.com>
2015-03-26 17:37:08 -0300

    banners mobile

--- js/banners.js
+++ js/banners.js
@@ -1,184 +1,186 @@
 ;(function (win) {
 
-	/* Banner Experiencia */
+	if( $(window).width() > 1024 ) {
+	
+		/* Banner Experiencia */
 
-	animationController.addTween(
-		'.banner-experiencia',
-	    TweenMax.to(
-	    	$('.banner-experiencia .a1'),
-	    	.5,
-	    	{css: {left: '-100px'}}, {css: {left:'-200px'}}
-	    ),
-	    300
-	);
+		animationController.addTween(
+			'.banner-experiencia',
+		    TweenMax.to(
+		    	$('.banner-experiencia .a1'),
+		    	.5,
+		    	{css: {left: '-100px'}}, {css: {left:'-200px'}}
+		    ),
+		    300
+		);
 
-	animationController.addTween(
-		'.banner-experiencia',
-	    TweenMax.to(
-	    	$('.banner-experiencia .a2'),
-	    	.5,
-	    	{css: {left: '-100px'}}, {css: {left:'-200px'}}
-	    ),
-	    300
-	);
+		animationController.addTween(
+			'.banner-experiencia',
+		    TweenMax.to(
+		    	$('.banner-experiencia .a2'),
+		    	.5,
+		    	{css: {left: '-100px'}}, {css: {left:'-200px'}}
+		    ),
+		    300
+		);
 
-	animationController.addTween(
-		'.banner-experiencia',
-	    TweenMax.to(
-	    	$('.banner-experiencia .a3'),
-	    	.5,
-	    	{css: {top: '-100px'}}, {css: {top:'-200px'}}
-	    ),
-	    300
-	);
+		animationController.addTween(
+			'.banner-experiencia',
+		    TweenMax.to(
+		    	$('.banner-experiencia .a3'),
+		    	.5,
+		    	{css: {top: '-100px'}}, {css: {top:'-200px'}}
+		    ),
+		    300
+		);
 
-	animationController.addTween(
-		'.banner-experiencia',
-	    TweenMax.to(
-	    	$('.banner-experiencia .a4'),
-	    	.5,
-	    	{css: {right: '-100px'}}, {css: {right:'-200px'}}
-	    ),
-	    300
-	);
-
-
-	/* banner-futuro */
-
-	animationController.addTween(
-		'.banner-futuro',
-	    TweenMax.to(
-	    	$('.banner-futuro .a1'),
-	    	.5,
-	    	{css: {left: '-100px'}}, {css: {left:'-200px'}}
-	    ),
-	    300
-	);
-
-	animationController.addTween(
-		'.banner-futuro',
-	    TweenMax.to(
-	    	$('.banner-futuro .a2'),
-	    	.5,
-	    	{css: {left: '-100px'}}, {css: {left:'-200px'}}
-	    ),
-	    300
-	);
-
-	animationController.addTween(
-		'.banner-futuro',
-	    TweenMax.to(
-	    	$('.banner-futuro .a3'),
-	    	.5,
-	    	{css: {top: '-100px'}}, {css: {top:'-200px'}}
-	    ),
-	    300
-	);
-
-	animationController.addTween(
-		'.banner-futuro',
-	    TweenMax.to(
-	    	$('.banner-futuro .a4'),
-	    	.5,
-	    	{css: {right: '-100px'}}, {css: {right:'-200px'}}
-	    ),
-	    300
-	);
-
-	animationController.addTween(
-		'.banner-futuro',
-	    TweenMax.to(
-	    	$('.banner-futuro .a5'),
-	    	.5,
-	    	{css: {right: '-100px'}}, {css: {right:'-200px'}}
-	    ),
-	    300
-	);
+		animationController.addTween(
+			'.banner-experiencia',
+		    TweenMax.to(
+		    	$('.banner-experiencia .a4'),
+		    	.5,
+		    	{css: {right: '-100px'}}, {css: {right:'-200px'}}
+		    ),
+		    300
+		);
 
 
-	/* banner-ideas-innovadoras */
+		/* banner-futuro */
 
-	animationController.addTween(
-		'.banner-ideas-innovadoras',
-	    TweenMax.to(
-	    	$('.banner-ideas-innovadoras .a1'),
-	    	.5,
-	    	{css: {left: '-100px'}}, {css: {left:'-200px'}}
-	    ),
-	    300
-	);
+		animationController.addTween(
+			'.banner-futuro',
+		    TweenMax.to(
+		    	$('.banner-futuro .a1'),
+		    	.5,
+		    	{css: {left: '-100px'}}, {css: {left:'-200px'}}
+		    ),
+		    300
+		);
 
-	animationController.addTween(
-		'.banner-ideas-innovadoras',
-	    TweenMax.to(
-	    	$('.banner-ideas-innovadoras .a2'),
-	    	.5,
-	    	{css: {left: '-100px'}}, {css: {left:'-200px'}}
-	    ),
-	    300
-	);
+		animationController.addTween(
+			'.banner-futuro',
+		    TweenMax.to(
+		    	$('.banner-futuro .a2'),
+		    	.5,
+		    	{css: {left: '-100px'}}, {css: {left:'-200px'}}
+		    ),
+		    300
+		);
 
-	animationController.addTween(
-		'.banner-ideas-innovadoras',
-	    TweenMax.to(
-	    	$('.banner-ideas-innovadoras .a3'),
-	    	.5,
-	    	{css: {top: '-100px'}}, {css: {top:'-200px'}}
-	    ),
-	    300
-	);
+		animationController.addTween(
+			'.banner-futuro',
+		    TweenMax.to(
+		    	$('.banner-futuro .a3'),
+		    	.5,
+		    	{css: {top: '-100px'}}, {css: {top:'-200px'}}
+		    ),
+		    300
+		);
 
-	animationController.addTween(
-		'.banner-ideas-innovadoras',
-	    TweenMax.to(
-	    	$('.banner-ideas-innovadoras .a4'),
-	    	.5,
-	    	{css: {right: '-100px'}}, {css: {right:'-200px'}}
-	    ),
-	    300
-	);
+		animationController.addTween(
+			'.banner-futuro',
+		    TweenMax.to(
+		    	$('.banner-futuro .a4'),
+		    	.5,
+		    	{css: {right: '-100px'}}, {css: {right:'-200px'}}
+		    ),
+		    300
+		);
 
-	animationController.addTween(
-		'.banner-ideas-innovadoras',
-	    TweenMax.to(
-	    	$('.banner-ideas-innovadoras .a5'),
-	    	.5,
-	    	{css: {right: '-100px'}}, {css: {right:'-200px'}}
-	    ),
-	    300
-	);
+		animationController.addTween(
+			'.banner-futuro',
+		    TweenMax.to(
+		    	$('.banner-futuro .a5'),
+		    	.5,
+		    	{css: {right: '-100px'}}, {css: {right:'-200px'}}
+		    ),
+		    300
+		);
 
-	animationController.addTween(
-		'.banner-ideas-innovadoras',
-	    TweenMax.to(
-	    	$('.banner-ideas-innovadoras .a6'),
-	    	.5,
-	    	{css: {top: '-100px'}}, {css: {top:'-200px'}}
-	    ),
-	    300
-	);
 
-	/* banner-casos exito */
+		/* banner-ideas-innovadoras */
 
-	animationController.addTween(
-		'.banner-casos',
-	    TweenMax.to(
-	    	$('.banner-casos .a1'),
-	    	.5,
-	    	{css: {left: '-300px'}}, {css: {left:'-200px'}}
-	    ),
-	    300
-	);
+		animationController.addTween(
+			'.banner-ideas-innovadoras',
+		    TweenMax.to(
+		    	$('.banner-ideas-innovadoras .a1'),
+		    	.5,
+		    	{css: {left: '-100px'}}, {css: {left:'-200px'}}
+		    ),
+		    300
+		);
 
-	animationController.addTween(
-		'.banner-casos',
-	    TweenMax.to(
-	    	$('.banner-casos .a2'),
-	    	.5,
-	    	{css: {right: '-300px'}}, {css: {right:'-200px'}}
-	    ),
-	    300
-	);
+		animationController.addTween(
+			'.banner-ideas-innovadoras',
+		    TweenMax.to(
+		    	$('.banner-ideas-innovadoras .a2'),
+		    	.5,
+		    	{css: {left: '-100px'}}, {css: {left:'-200px'}}
+		    ),
+		    300
+		);
 
+		animationController.addTween(
+			'.banner-ideas-innovadoras',
+		    TweenMax.to(
+		    	$('.banner-ideas-innovadoras .a3'),
+		    	.5,
+		    	{css: {top: '-100px'}}, {css: {top:'-200px'}}
+		    ),
+		    300
+		);
+
+		animationController.addTween(
+			'.banner-ideas-innovadoras',
+		    TweenMax.to(
+		    	$('.banner-ideas-innovadoras .a4'),
+		    	.5,
+		    	{css: {right: '-100px'}}, {css: {right:'-200px'}}
+		    ),
+		    300
+		);
+
+		animationController.addTween(
+			'.banner-ideas-innovadoras',
+		    TweenMax.to(
+		    	$('.banner-ideas-innovadoras .a5'),
+		    	.5,
+		    	{css: {right: '-100px'}}, {css: {right:'-200px'}}
+		    ),
+		    300
+		);
+
+		animationController.addTween(
+			'.banner-ideas-innovadoras',
+		    TweenMax.to(
+		    	$('.banner-ideas-innovadoras .a6'),
+		    	.5,
+		    	{css: {top: '-100px'}}, {css: {top:'-200px'}}
+		    ),
+		    300
+		);
+
+		/* banner-casos exito */
+
+		animationController.addTween(
+			'.banner-casos',
+		    TweenMax.to(
+		    	$('.banner-casos .a1'),
+		    	.5,
+		    	{css: {left: '-300px'}}, {css: {left:'-200px'}}
+		    ),
+		    300
+		);
+
+		animationController.addTween(
+			'.banner-casos',
+		    TweenMax.to(
+		    	$('.banner-casos .a2'),
+		    	.5,
+		    	{css: {right: '-300px'}}, {css: {right:'-200px'}}
+		    ),
+		    300
+		);
+	}
 
 }(window));
